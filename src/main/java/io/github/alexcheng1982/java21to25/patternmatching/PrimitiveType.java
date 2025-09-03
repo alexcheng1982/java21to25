6b7af32f193143d41d5d729ff31e35ea7d4c8d8a
Fu Cheng <alexcheng1982@gmail.com>
2025-09-03 23:06:09 +1200

    pattern matching

---
+++ src/main/java/io/github/alexcheng1982/java21to25/patternmatching/PrimitiveType.java
@@ -0,0 +1,5 @@
+package io.github.alexcheng1982.java21to25.patternmatching;
+
+public class PrimitiveType {
+
+}
---
+++ src/main/java/io/github/alexcheng1982/java21to25/patternmatching/TypePattern.java
@@ -0,0 +1,17 @@
+package io.github.alexcheng1982.java21to25.patternmatching;
+
+public class TypePattern {
+  static void test(int i) {
+    switch (i) {
+      case byte b -> System.out.println("byte: " + b);
+      case int x when x > 1000 -> System.out.println("large int: " + x);
+      default -> System.out.println("small int: " + i);
+    }
+  }
+
+  static void main() {
+    test(1);
+    test(300);
+    test(2000);
+  }
+}
