9e50f4c56e75ab7414a0d81997b410dc60eead6b
Fu Cheng <alexcheng1982@gmail.com>
2025-09-09 09:25:58 +1200

    scoped value

---
+++ src/main/java/io/github/alexcheng1982/java21to25/scopedvalue/SimpleScopedValue.java
@@ -0,0 +1,16 @@
+package io.github.alexcheng1982.java21to25.scopedvalue;
+
+public class SimpleScopedValue {
+
+    private final static ScopedValue<String> VALUE = ScopedValue.newInstance();
+
+    public void print() {
+        ScopedValue.where(VALUE, "hello").run(() -> {
+            System.out.println(VALUE.get());
+        });
+    }
+
+    static void main() {
+        new SimpleScopedValue().print();
+    }
+}
